242b65175942b56c1f18667a73e15e784f8a6d21
ChronoKeeper <norwinofficial@gmail.com>
2025-01-09 20:08:24 +0100

    Utworzenie przycisków i pól do wpisywania danych dla interfejsu graficznego

--- ResourceManagementSystem/Program.cs
+++ ResourceManagementSystem/Program.cs
@@ -22,11 +22,59 @@ namespace ResourceManagementSystem
 
     public class MainForm : Form
     {
+
+        private ResourceManager _resourceManager;
+
+        private ListBox _resourceList;
+
+        private TextBox _resourceNameBox;
+        private TextBox _resourceLocationBox;
+        private TextBox _userNameBox;
+
+        private Button _addResourceButton;
+        private Button _allocateButton;
+        private Button _releaseButton;
+
         public MainForm()
         {
-            Text = "Resource Management System";
+            _resourceManager = new ResourceManager();
+
+            Text = "System Zarządzania Zasobami";
             Width = 600;
             Height = 400;
+
+            _resourceList = new ListBox { Top = 10, Left = 10, Width = 560, Height = 200 };
+            Controls.Add(_resourceList);
+
+            var resourceNameLabel = new Label { Text = "Nazwa zasobu:", Top = 220, Left = 10, Width = 100 };
+            Controls.Add(resourceNameLabel);
+
+            _resourceNameBox = new TextBox { Top = 220, Left = 120, Width = 200 };
+            Controls.Add(_resourceNameBox);
+
+            var resourceLocationLabel = new Label { Text = "Lokalizacja zasobu:", Top = 250, Left = 10, Width = 100 };
+            Controls.Add(resourceLocationLabel);
+
+            _resourceLocationBox = new TextBox { Top = 250, Left = 120, Width = 200 };
+            Controls.Add(_resourceLocationBox);
+
+            var userNameLabel = new Label { Text = "Użytkownik:", Top = 280, Left = 10, Width = 100 };
+            Controls.Add(userNameLabel);
+
+            _userNameBox = new TextBox { Top = 280, Left = 120, Width = 200 };
+            Controls.Add(_userNameBox);
+
+            _addResourceButton = new Button { Text = "Dodaj zasób", Top = 310, Left = 10, Width = 100 };
+            Controls.Add(_addResourceButton);
+
+            _allocateButton = new Button { Text = "Przydziel", Top = 310, Left = 120, Width = 100 };
+            Controls.Add(_allocateButton);
+
+            _releaseButton = new Button { Text = "Zwolnij", Top = 310, Left = 230, Width = 100 };
+            Controls.Add(_releaseButton);
+
         }
+
+
     }
 }
